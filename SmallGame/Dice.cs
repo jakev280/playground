e14a462bc6de373ec341495568774402fa1bf1bc
jakev280 <jakevirdee@gmail.com>
2025-12-27 15:08:48 +0000

    art

--- SmallGame/Dice.cs
+++ SmallGame/Dice.cs
@@ -6,11 +6,13 @@ namespace TinyDungeon
     {
         static Random rng = new Random();
 
+        // Roll a single dice of given sides
         public static int D(int sides)
         {
             return rng.Next(1, sides + 1);
         }
 
+        // Roll multiple dice
         public static int Roll(int count, int sides)
         {
             int total = 0;
